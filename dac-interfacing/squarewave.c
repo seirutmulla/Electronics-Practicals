a98e6af607a188a8e5ff85106a429927193c3c0a
Seirut Mulla <mullaseirut@gmail.com>
2026-07-23 20:11:01 +0530

    Add squarewave.c for DAC interfacing
    Implement a square wave generator using PORTB.

---
+++ dac-interfacing/squarewave.c
@@ -0,0 +1,23 @@
+#include<xc.h>
+#pragma config OSC = HS // use high-speed external osc crystal
+#pragma config PWRT = OFF
+#pragma config WDT=OFF
+#pragma config DEBUG = OFF
+#pragma config LVP=OFF
+void main()
+{
+    unsigned char i;
+    TRISB=0X00;
+    PORTB=0X00;
+    while(1)
+    {
+        for(i=0;i<255;i++)
+        {
+            PORTB=0XFF;
+        }
+        for(i=255;i>0;i--)
+        {
+            PORTB=0X00;
+        }
+    }
+}
